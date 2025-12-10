8bde695ddf9b3089fead2cef868d56d841b9d77c
martin-selise <hasnat.martin@selisegroup.com>
2025-12-10 20:32:15 +0600

    updated

--- src/components/ui/BlogCard.tsx
+++ src/components/ui/BlogCard.tsx
@@ -9,7 +9,6 @@ import Link from "next/link";
 interface CardStoryblok extends SbBlokData {
 	blog_card_image?: { filename?: string; alt?: string };
 	blog_card_title?: string;
-	blog_card_description?: string;
 	blog_card_url?: { url?: string };
 	blog_card_btn_text?: string;
 }
