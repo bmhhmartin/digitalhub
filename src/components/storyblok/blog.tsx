cf2bbc8a69013431fb98849388734afee05157ae
martin-selise <hasnat.martin@selisegroup.com>
2025-12-10 19:24:44 +0600

    blog design updated

--- src/components/storyblok/blog.tsx
+++ src/components/storyblok/blog.tsx
@@ -1,22 +1,25 @@
+import type { Blog as BlogStoryblok } from "@/.storyblok/types/287474179047807/storyblok-components";
 import {
-	renderRichText,
-	type SbBlokData,
-	storyblokEditable,
+    renderRichText,
+    type SbBlokData,
+    storyblokEditable,
 } from "@storyblok/react/rsc";
 import parse from "html-react-parser";
-import type { Blog as BlogStoryblok } from "@/.storyblok/types/287474179047807/storyblok-components";
 
 const Blog = ({ blok }: { blok: BlogStoryblok }) => {
 	return (
-		<div
+		<section className="container mx-auto px-4 lg:px-0 py-20">
+            <div
 			{...storyblokEditable(blok as SbBlokData)}
-			className="flex flex-col items-center justify-center bg-gray-200"
-		>
-			<h1 className="text-4xl">{blok?.title}</h1>
-			<div className="richtext">
-				{parse(renderRichText(blok?.description) ?? "")}
-			</div>
-		</div>
+			className="flex flex-col items-center justify-center"
+            >
+                
+                <div className="richtext">
+                    {parse(renderRichText(blok?.description) ?? "")}
+                </div>
+                <h1 className="text-4xl font-bold">{blok?.title}</h1>
+            </div>
+        </section>
 	);
 };
 
